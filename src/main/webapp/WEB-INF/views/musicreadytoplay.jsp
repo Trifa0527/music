7c11c2515011739d27f3c3c86ed8da3d7af76199
User <User@DESKTOP-FHSPKR7>
2022-01-28 17:35:52 +0900

    2022.01.28 RDTP div, anime

--- src/main/webapp/WEB-INF/views/musicreadytoplay.jsp
+++ src/main/webapp/WEB-INF/views/musicreadytoplay.jsp
@@ -7,21 +7,32 @@
 <link rel="shortcut icon" type="image/x-icon" href="resources/logo.ico">
 <link rel="stylesheet" href="resources/css/rdtp.css">
 <link rel="stylesheet" href="resources/css/rdtpobj.css">
+<script src="resources/js/rdtp.js"></script>
 <title>Trifa's Music</title>
 </head>
 <body style="background-color: #221436;">
 	<div id="left">
+		<div class="lct">
 		
+		</div>
 	</div>
 	<div id="selected">
-		<div id="pic">
-		</div>
-		<div id="info">
-		
+		<div class="sct">
+			<div class="pic">
+				<img class="alpic" src="resources/mis.png">
+			</div>
+			<div class="info">
+			
+			</div>
+			<div class="intro">
+				
+			</div>
 		</div>
 	</div>
 	<div id="right">
+		<div class="rct">
 		
+		</div>
 	</div>
 </body>
 </html>
---
+++ src/main/webapp/resources/js/rdtp.js
@@ -0,0 +1,49 @@
+function fadeOut(target, ma) {
+	var level = 1;
+	var outTimer = null;
+	outTimer = setInterval( function() {
+		level = fadeOutAction(target, level, outTimer, ma);
+	}, 50);
+}
+function fadeOutAction(target, level, outTimer, ma) {
+	level = level - ma;
+	changeOpacity(target, level);
+	if(level < 0) {
+		clearInterval(outTimer);
+	}
+	return level;
+}
+function fadeIn(target, ma) {
+	var level = 0;
+	var inTimer = null;
+	inTimer = setInterval( function() {
+		level = fadeInAction(target, level, inTimer, ma);
+	}, 50);
+}
+function fadeInAction(target, level, inTimer, ma) {
+	level = level + ma;
+	changeOpacity(target, level);
+	if(level>1) clearInterval(inTimer);
+	return level;
+}
+function changeOpacity(target, level) {
+	var obj = target;
+	obj.style.opacity = level;
+	obj.style.MozOpacity = level;
+	obj.style.KhtmlOpacity = level;
+	obj.style.MsFilter = "'progid: DXImageTransform.Microsoft.Alpha(Opacity=" + (level * 100) + ")'";
+	obj.style.filter = "alpha(opacity=" + (level * 100) + ");";
+}
+
+setTimeout(function(){
+    var sted = document.getElementById("selected");
+	fadeIn(sted, 0.1);
+	setTimeout(function(){
+    	var left = document.getElementById("left");
+		fadeIn(left, 0.1);
+		setTimeout(function(){
+    		var right = document.getElementById("right");
+			fadeIn(right, 0.1);
+		}, 800);
+	}, 800);
+}, 0);
